dc609da1ecb22ffb9f48f28cca8dc11f7a884274
John Reese <john@noswap.com>
2015-02-03 11:53:52 -0800

    Barebones module and build structure

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+from os import path
+import shutil
+
+if path.isfile('README.md'):
+    shutil.copyfile('README.md', 'README')
+
+setup(
+    name='systemwatch',
+    description='logwatch for systemd/journald',
+    version='0.1.0',
+    author='John Reese',
+    author_email='john@noswap.com',
+    url='https://github.com/jreese/systemwatch',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Logging',
+        'Topic :: Utilities',
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+    license='MIT License',
+    packages=['systemwatch'],
+    scripts=['bin/systemwatch'],
+)
---
+++ systemwatch/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2015 John Reese
+# Licensed under the MIT license
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__version__ = '0.1.0'
